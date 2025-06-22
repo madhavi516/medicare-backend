d8eca1d917ef54a62a8ded6eff93f239247ab8ce
madhavi <nakkalamadhavi8@gmail.com>
2025-06-22 20:34:58 +0530

    fix: use dynamic PORT for Render

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ import { initDB } from './models/initDB.js';
 import fileUpload from './routes/uploadRoutes.js';
 
 const app = express();
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
